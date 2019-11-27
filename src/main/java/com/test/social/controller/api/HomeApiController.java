46b56923a222d3feab5dd54c22ba91cb8b7102cb
Rajesh Saha <rajesh@workspaceit.com>
2019-11-27 12:01:27 +0600

    change

--- src/main/java/com/test/social/controller/api/HomeApiController.java
+++ src/main/java/com/test/social/controller/api/HomeApiController.java
@@ -1,13 +1,19 @@
 package com.test.social.controller.api;
 
 import com.test.social.dao.UserDao;
+import com.test.social.model.viewmodels.LoginForm;
+import com.test.social.model.viewmodels.SignupForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,6 +47,22 @@ public class HomeApiController {
             data = body;
         } else return new ResponseEntity<ApiStatus>(HttpStatus.BAD_REQUEST);
 
+        return new ApiStatus(HttpStatus.OK,"Post Method.",data);
+    }
+
+
+    @RequestMapping(value = "/check", method = RequestMethod.POST)
+    public Object post(@ModelAttribute("loginForm") @Valid LoginForm loginForm, BindingResult result){
+        Map data = new HashMap();
+        if(result.hasErrors())
+        {
+            Map errMsg = new HashMap();
+            for(ObjectError error:result.getAllErrors()){
+                FieldError fe = (FieldError) error;
+                errMsg.put(fe.getField(), fe.getDefaultMessage());
+            }
+            return new ApiStatus(HttpStatus.BAD_REQUEST,"Error!",errMsg);
+        }
         return new ApiStatus(HttpStatus.OK,"Post Method.",data);
     }
 
